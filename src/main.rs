a42aa0aa0a53e59beae58c2102f87f7c033279d3
student <student@usue.ru>
2024-10-24 15:32:45 +0500

    refactoring-commit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use input_macro::input;
+use {input_macro::input, std::convert::TryInto};
 mod exit;
 mod wall;
 
@@ -42,6 +42,66 @@ enum Action {
     Exit,
 }
 
+fn get_direction_offset(direction: Direction) -> [(isize, isize); 13] {
+    let base_offsets = [
+        (2, -3),
+        (-2, -3),
+        (-1, -3),
+        (1, -3),
+        (0, -3),
+        (-1, -2),
+        (1, -2),
+        (0, -2),
+        (-1, -1),
+        (1, -1),
+        (0, -1),
+        (-1, 0),
+        (1, 0),
+    ];
+    match direction {
+        Direction::North => base_offsets,
+        Direction::South => base_offsets.map(|(x, y)| (-x, -y)),
+        Direction::East => base_offsets.map(|(x, y)| (-y, x)),
+        Direction::West => base_offsets.map(|(x, y)| (y, -x)),
+    }
+}
+
+fn get_direction(direction: Direction) -> (isize, isize) {
+    match direction {
+        Direction::North => (0, -1),
+        Direction::East => (1, 0),
+        Direction::South => (0, 1),
+        Direction::West => (-1, 0),
+    }
+}
+
+fn try_move_player(player: &Player, dx: isize, dy: isize) -> (usize, usize, Block) {
+    let x = player.x.checked_add_signed(dx);
+    let y = player.y.checked_add_signed(dy);
+    match (x, y) {
+        (Some(x), Some(y)) if x < 5 && y < 5 => (x, y, MAP[y][x]),
+        _ => (0, 0, Block::Wall),
+    }
+}
+
+fn rotate_left(direction: Direction) -> Direction {
+    match direction {
+        Direction::North => Direction::West,
+        Direction::East => Direction::North,
+        Direction::South => Direction::East,
+        Direction::West => Direction::South,
+    }
+}
+
+fn rotate_right(direction: Direction) -> Direction {
+    match direction {
+        Direction::North => Direction::East,
+        Direction::West => Direction::North,
+        Direction::South => Direction::West,
+        Direction::East => Direction::South,
+    }
+}
+
 fn main() {
     let mut player = Player {
         x: 0,
@@ -57,22 +117,12 @@ fn main() {
             println!("Стена!");
             action = Action::None;
         }
+
         let input = input!("> ");
         match input.as_str() {
             "w" => {
-                let (dx, dy) = match player.direction {
-                    Direction::North => (0, -1),
-                    Direction::East => (1, 0),
-                    Direction::South => (0, 1),
-                    Direction::West => (-1, 0),
-                };
-
-                let x = player.x.checked_add_signed(dx);
-                let y = player.y.checked_add_signed(dy);
-                let (x, y, block) = match (x, y) {
-                    (Some(x), Some(y)) if x < 5 && y < 5 => (x, y, MAP[y][x]),
-                    _ => (0, 0, Block::Wall),
-                };
+                let (dx, dy) = get_direction(player.direction);
+                let (x, y, block) = try_move_player(&player, dx, dy);
 
                 match block {
                     Block::Air => {
@@ -89,22 +139,11 @@ fn main() {
                 }
             },
             "a" => {
-                player.direction = match player.direction {
-                    Direction::North => Direction::West,
-                    Direction::East => Direction::North,
-                    Direction::South => Direction::East,
-                    Direction::West => Direction::South,
-                };
-
+                player.direction = rotate_left(player.direction);
                 action = Action::Rotate;
             },
             "d" => {
-                player.direction = match player.direction {
-                    Direction::North => Direction::East,
-                    Direction::West => Direction::North,
-                    Direction::South => Direction::West,
-                    Direction::East => Direction::South,
-                };
+                player.direction = rotate_right(player.direction);
                 action = Action::Rotate;
             },
             _ => {},
@@ -113,85 +152,12 @@ fn main() {
 }
 
 fn render(player: &Player) {
-    let var_name = match player.direction {
-        Direction::North => {
-            [
-                (0, -2, -3),
-                (1, 2, -3),
-                (2, -1, -3),
-                (3, 0, -3),
-                (4, 1, -3),
-                (5, -1, -2),
-                (6, 1, -2),
-                (7, 0, -2),
-                (8, -1, -1),
-                (9, 1, -1),
-                (10, 0, -1),
-                (11, -1, 0),
-                (12, 1, 0),
-            ]
-        },
-        Direction::South => {
-            [
-                (0, 2, 3),
-                (1, -2, 3),
-                (2, 1, 3),
-                (3, -1, 3),
-                (4, 0, 3),
-                (5, 1, 2),
-                (6, -1, 2),
-                (7, 0, 2),
-                (8, 1, 1),
-                (9, -1, 1),
-                (10, 0, 1),
-                (11, 1, 0),
-                (12, -1, 0),
-            ]
-        },
-        Direction::East => {
-            [
-                (0, 3, 2),
-                (1, 3, -2),
-                (2, 3, 1),
-                (3, 3, -1),
-                (4, 3, 0),
-                (5, 2, 2),
-                (6, 2, -2),
-                (7, 2, 0),
-                (8, 1, -1),
-                (9, 1, 1),
-                (10, 1, 0),
-                (11, 0, -1),
-                (12, 0, 1),
-            ]
-        },
-        Direction::West => {
-            [
-                (0, -3, -2),
-                (1, -3, 2),
-                (2, -3, -1),
-                (3, -3, 1),
-                (4, -3, 0),
-                (5, -2, -1),
-                (6, -2, 1),
-                (7, -2, 0),
-                (8, -1, -1),
-                (9, -1, 1),
-                (10, -1, 0),
-                (11, 0, -1),
-                (12, 0, 1),
-            ]
-        },
-    };
-    let positions = var_name;
+    let offsets = get_direction_offset(player.direction);
     let mut frame = [[' '; 26]; 14];
-    for (i, dx, dy) in positions {
-        let x = player.x.checked_add_signed(dx);
-        let y = player.y.checked_add_signed(dy);
-        let (_, _, block) = match (x, y) {
-            (Some(x), Some(y)) if x < 5 && y < 5 => (x, y, MAP[y][x]),
-            _ => (0, 0, Block::Wall),
-        };
+    #[expect(unused_variables)]
+    for (i, (dx, dy)) in offsets.into_iter().enumerate() {
+        let (x, y, block) = try_move_player(player, dx, dy);
+
         let pattern = match block {
             Block::Air => continue,
             Block::Wall => wall::PATTERNS[i * 4 + player.direction as usize],
@@ -241,19 +207,9 @@ mod tests {
         };
         #[expect(unused_assignments)]
         let mut action = Action::None;
-        let (dx, dy) = match player.direction {
-            Direction::North => (0, -1),
-            Direction::East => (1, 0),
-            Direction::South => (0, 1),
-            Direction::West => (-1, 0),
-        };
 
-        let x = player.x.checked_add_signed(dx);
-        let y = player.y.checked_add_signed(dy);
-        let (x, y, block) = match (x, y) {
-            (Some(x), Some(y)) if x < 5 && y < 5 => (x, y, MAP[y][x]),
-            _ => (0, 0, Block::Wall),
-        };
+        let (dx, dy) = get_direction(player.direction);
+        let (x, y, block) = try_move_player(&player, dx, dy);
 
         match block {
             Block::Air => {
